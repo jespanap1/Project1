df7ffd3deeb2cf814f243ba5c6c928ab7fbf8fa3
jespanap1 <jespanap@eafit.edu.co>
2023-09-16 21:33:58 -0500

    no usaremos aun eso

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 const registerController = require('../controllers/registerController');
 
+// @GET /products
 router.get('/', registerController.getRegister);
+// @POST /products
+router.post('/', registerController.postRegister);
+
+
+
 
 
 module.exports = router;
